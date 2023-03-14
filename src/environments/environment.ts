77c7cead220a3fe7640528083d81991e2bae5fa4
musto <musti.sar4@gmail.com>
2023-03-14 02:29:18 +0100

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,22 @@
-import * as dotenv from "dotenv";
-dotenv.config();
+import {
+  FIREBASE_URL,
+  FIREBASE_API_KEY,
+  FIREBASE_COLLECTION_USERS,
+  FIREBASE_COLLECTION_TASKS,
+} from "../../.env";
 
 export const environment = {
   production: true,
-  firebaseUrl: process.env["FIREBASE_URL"],
-  firebaseApiKey: process.env["FIREBASE_API_KEY"],
+  firebaseUrl: FIREBASE_URL,
+  firebaseApiKey: FIREBASE_API_KEY,
+  authEndpointSignUp:
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp",
+  authEndpointSignIn:
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword",
+  authEndpointDeleteUserAccount:
+    "https://identitytoolkit.googleapis.com/v1/accounts:delete",
+  firebaseCollections: {
+    collectionUsers: FIREBASE_COLLECTION_USERS,
+    collectionTasks: FIREBASE_COLLECTION_TASKS,
+  },
 };
