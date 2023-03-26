cb1f35c26ac35bd2e21a4cda1bd49328b804a29a
musto <musti.sar4@gmail.com>
2023-03-26 21:19:01 +0200

    Update styles

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,26 +2,26 @@ export const LOGIN_SIGNUP_FORM_STYLE = {
   width: "80%",
   minWidth: "250px",
   maxWidth: "480px",
-  disableClose: true,
+  disableClose: false,
 };
 
 export const CONFIRMATION_POPUP_STYLE = {
   width: "80%",
   minWidth: "250px",
   maxWidth: "480px",
-  disableClose: true,
+  disableClose: false,
 };
 
 export const TASK_ADD_FORM_STYLE = {
   width: "80%",
   minWidth: "250px",
   maxWidth: "480px",
-  disableClose: true,
+  disableClose: false,
 };
 
 export const TASK_DETAILS_FORM_STYLE = {
   width: "80%",
   minWidth: "250px",
   maxWidth: "480px",
-  disableClose: true,
+  disableClose: false,
 };
